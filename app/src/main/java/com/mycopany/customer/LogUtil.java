e653c022f2530ad76c1bf0a6f175987c08d4d051
llpin <llpin07@163.com>
2017-07-02 23:42:32 +0800

    二维码修改 增加短信验证功能

---
+++ app/src/main/java/com/mycopany/customer/LogUtil.java
@@ -0,0 +1,8 @@
+package com.mycopany.customer;
+
+/**
+ * Created by linlipin on 17/7/2.
+ */
+
+public class LogUtil {
+}
---
+++ app/src/main/java/com/mycopany/customer/SMSActivity.java
@@ -0,0 +1,13 @@
+package com.mycopany.customer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SMSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms);
+    }
+}
